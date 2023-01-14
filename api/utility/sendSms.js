f64cdecc9bcde957e3fa423668e9828d4d14de60
rohan-mostofa <rohanmostofa599@gmail.com>
2023-01-14 20:20:22 +0600

    seventh commit

--- api/utility/sendSms.js
+++ api/utility/sendSms.js
@@ -4,7 +4,7 @@ export const sendSms = async (phone, sms) => {
 
     try{
 
-        await axios.get(`https://bulksmsbd.net/api/smsapi?api_key=${process.env.SMS_API_KEY}&type=text&number=${phone}&senderid${process.env.SMS_SENDER_ID}&message=${sms}`)
+        await axios.get(``)
 
     }catch(err){
         console.log(err);
